87f094d96b9f78a8f616a69adbf7746a940efd28
Achim Pieters <achimpieters@hotmail.com>
2020-03-16 17:21:44 +0100

    Update battery.c

--- battery.c
+++ battery.c
@@ -1,5 +1,25 @@
+/** Copyright 2019 Achim Pieters | StudioPieters®
 
-/*************************** STATUS_LOW_BATTERY ***************************/
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+   Build upon: ESP-HomeKit - MIT License - Copyright (c) 2017 Maxim Kulkin
+ **/
+
 homekit_characteristic_t status_low_battery = HOMEKIT_CHARACTERISTIC_(STATUS_LOW_BATTERY, 0);
 
 
@@ -31,77 +51,3 @@ void battery_low_init() {
 
 
 battery_low_init();
-
-/*************************** STATUS_LOW_BATTERY ***************************/
-
-/**
-   Defines that the accessory contains a battery that can be monitored.
-   Required Characteristics:
-   - BATTERY_LEVEL
-   - CHARGING_STATE
-   - STATUS_LOW_BATTERY
-
-   Optional Characteristics:
-   - NAME
- **/
-#define HOMEKIT_SERVICE_BATTERY_SERVICE HOMEKIT_APPLE_UUID2("96")
-
-#define HOMEKIT_CHARACTERISTIC_BATTERY_LEVEL HOMEKIT_APPLE_UUID2("68")
-#define HOMEKIT_DECLARE_CHARACTERISTIC_BATTERY_LEVEL(_value, ...) \
-        .type = HOMEKIT_CHARACTERISTIC_BATTERY_LEVEL, \
-        .description = "Battery Level", \
-        .format = homekit_format_uint8, \
-        .unit = homekit_unit_percentage, \
-        .permissions = homekit_permissions_paired_read \
-                       | homekit_permissions_notify, \
-        .min_value = (float[]) {0}, \
-        .max_value = (float[]) {100}, \
-        .min_step = (float[]) {1}, \
-        .value = HOMEKIT_UINT8_(_value), \
-        ## __VA_ARGS__
-
-
-#define HOMEKIT_CHARACTERISTIC_CHARGING_STATE HOMEKIT_APPLE_UUID2("8F")
-#define HOMEKIT_DECLARE_CHARACTERISTIC_CHARGING_STATE(_value, ...) \
-        .type = HOMEKIT_CHARACTERISTIC_CHARGING_STATE, \
-        .description = "Charging State", \
-        .format = homekit_format_uint8, \
-        .permissions = homekit_permissions_paired_read \
-                       | homekit_permissions_notify, \
-        .min_value = (float[]) {0}, \
-        .max_value = (float[]) {2}, \
-        .min_step = (float[]) {1}, \
-        .valid_values = { \
-                .count = 3, \
-                .values = (uint8_t[]) { 0, 1, 2 }, \
-        }, \
-        .value = HOMEKIT_UINT8_(_value), \
-        ## __VA_ARGS__
-
-
-#define HOMEKIT_CHARACTERISTIC_STATUS_LOW_BATTERY HOMEKIT_APPLE_UUID2("79")
-#define HOMEKIT_DECLARE_CHARACTERISTIC_STATUS_LOW_BATTERY(_value, ...) \
-        .type = HOMEKIT_CHARACTERISTIC_STATUS_LOW_BATTERY, \
-        .description = "Status Low Battery", \
-        .format = homekit_format_uint8, \
-        .permissions = homekit_permissions_paired_read \
-                       | homekit_permissions_notify, \
-        .min_value = (float[]) {0}, \
-        .max_value = (float[]) {1}, \
-        .min_step = (float[]) {1}, \
-        .valid_values = { \
-                .count = 2, \
-                .values = (uint8_t[]) { 0, 1 }, \
-        }, \
-        .value = HOMEKIT_UINT8_(_value), \
-        ## __VA_ARGS__
-
-
-#define HOMEKIT_CHARACTERISTIC_NAME HOMEKIT_APPLE_UUID2("23")
-#define HOMEKIT_DECLARE_CHARACTERISTIC_NAME(name, ...) \
-        .type = HOMEKIT_CHARACTERISTIC_NAME, \
-        .description = "Name", \
-        .format = homekit_format_string, \
-        .permissions = homekit_permissions_paired_read, \
-        .value = HOMEKIT_STRING_(name), \
-        ## __VA_ARGS__
